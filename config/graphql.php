c9ec709088f9a577355ae1d4cc5e8b37d1e7b46f
Ranga Lakshitha <ranga@thesanmark.com>
2018-12-04 15:43:25 +0530

    query for tasks in action

--- config/graphql.php
+++ config/graphql.php
@@ -1,16 +1,11 @@
 <?php
 
-
-use example\Mutation\ExampleMutation;
-use example\Query\ExampleQuery;
-use example\Type\ExampleRelationType;
-use example\Type\ExampleType;
+use App\GraphQL\Query\TaskQuery;
+use \App\GraphQL\Type\TaskType;
 
 return [
-
     // The prefix for routes
     'prefix' => 'graphql',
-
     // The routes to make GraphQL request. Either a string that will apply
     // to both query and mutation or an array containing the key 'query' and/or
     // 'mutation' with the according Route
@@ -29,7 +24,6 @@ return [
     // ]
     //
     'routes' => '{graphql_schema?}',
-
     // The controller to use in GraphQL request. Either a string that will apply
     // to both query and mutation or an array containing the key 'query' and/or
     // 'mutation' with the according Controller and method
@@ -42,10 +36,8 @@ return [
     // ]
     //
     'controllers' => \Rebing\GraphQL\GraphQLController::class . '@query',
-
     // Any middleware for the graphql route group
     'middleware' => [],
-
     // Additional route group attributes
     //
     // Example:
@@ -53,12 +45,10 @@ return [
     // 'route_group_attributes' => ['guard' => 'api']
     //
     'route_group_attributes' => [],
-
     // The name of the default schema used when no argument is provided
     // to GraphQL::schema() or when the route is used without the graphql_schema
     // parameter.
     'default_schema' => 'default',
-
     // The schemas for query and/or mutation. It expects an array of schemas to provide
     // both the 'query' fields and the 'mutation' fields.
     //
@@ -100,16 +90,15 @@ return [
     'schemas' => [
         'default' => [
             'query' => [
-                'example_query' => ExampleQuery::class,
+                'tasks' => TaskQuery::class,
             ],
             'mutation' => [
-                'example_mutation'  => ExampleMutation::class,
+            //'example_mutation' => ExampleMutation::class,
             ],
             'middleware' => [],
             'method' => ['get', 'post'],
         ],
     ],
-
     // The types available in the application. You can then access it from the
     // facade like this: GraphQL::type('user')
     //
@@ -120,10 +109,8 @@ return [
     // ]
     //
     'types' => [
-        'example'           => ExampleType::class,
-        'relation_example'  => ExampleRelationType::class,
+        'Task' => TaskType::class,
     ],
-
     // This callable will be passed the Error object for each errors GraphQL catch.
     // The method should return an array representing the error.
     // Typically:
@@ -132,10 +119,8 @@ return [
     //     'locations' => []
     // ]
     'error_formatter' => ['\Rebing\GraphQL\GraphQL', 'formatError'],
-
     // You can set the key, which will be used to retrieve the dynamic variables
-    'params_key'    => 'variables',
-
+    'params_key' => 'variables',
     /*
      * Options to limit the query complexity and depth. See the doc
      * @ https://github.com/webonyx/graphql-php#security
@@ -146,21 +131,19 @@ return [
         'query_max_depth' => null,
         'disable_introspection' => false
     ],
-
     // You can define custom paginators to override the out-of-the-box fields
     // Useful if you want to inject some parameters of your own that apply at the top
     // level of the collection rather than to each instance returned. Can also use this
     // to add in more of the Laravel pagination data (e.g. last_page).
     'custom_paginators' => [
-        // 'my_custom_pagination' => \Path\To\Your\CustomPagination::class,
+    // 'my_custom_pagination' => \Path\To\Your\CustomPagination::class,
     ],
-
     /*
      * Config for GraphiQL (see (https://github.com/graphql/graphiql).
      */
     'graphiql' => [
         'prefix' => '/graphiql/{graphql_schema?}',
-        'controller' => \Rebing\GraphQL\GraphQLController::class.'@graphiql',
+        'controller' => \Rebing\GraphQL\GraphQLController::class . '@graphiql',
         'middleware' => [],
         'view' => 'graphql::graphiql',
         'display' => env('ENABLE_GRAPHIQL', true),
