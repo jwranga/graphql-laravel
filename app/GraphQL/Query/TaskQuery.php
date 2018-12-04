8f020fc846e46acca396c953e36e22141f635a3d
Ranga Lakshitha <ranga@thesanmark.com>
2018-12-04 14:57:26 +0530

    create the TaskQuery

---
+++ app/GraphQL/Query/TaskQuery.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\GraphQL\Query;
+
+use GraphQL;
+use GraphQL\Type\Definition\Type;
+use Rebing\GraphQL\Support\Query;
+use App\Task;
+
+class TaskQuery extends Query
+
+{
+	protected $attributes = [
+		'name'  => 'tasks'
+	];
+
+	public function type()
+	{
+		return Type::listOf(GraphQL::type('Task'));
+	}
+
+	public function resolve ($root, $args)
+	{
+		return Task::all();
+	}
+}
+
